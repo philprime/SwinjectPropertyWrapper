9a8b09c8be815bd4b69264ef5ffa7e4b200b3859
Philip Niedertscheider <me@philprime.dev>
2024-12-11 16:39:13 +0100

    refactor: replace Quick & Nimble with XCTest

--- Package.swift
+++ Package.swift
@@ -9,15 +9,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Swinject/Swinject", .upToNextMajor(from: "2.7.1")),
-        .package(url: "https://github.com/Quick/Quick", .upToNextMajor(from: "3.0.0")),
-        .package(url: "https://github.com/Quick/Nimble", .upToNextMajor(from: "9.0.0")),
+        .package(url: "https://github.com/mattgallagher/CwlPreconditionTesting.git", .upToNextMajor(from: "2.2.2"))
     ],
     targets: [
         .target(name: "SwinjectPropertyWrapper", dependencies: ["Swinject"]),
         .testTarget(name: "SwinjectPropertyWrapperTests", dependencies: [
             "SwinjectPropertyWrapper",
-            "Quick",
-            "Nimble"
+            .product(name: "CwlPreconditionTesting", package: "CwlPreconditionTesting"),
+            .product(name: "CwlPosixPreconditionTesting", package: "CwlpreconditionTesting")
         ]),
     ]
 )
--- Tests/SwinjectPropertyWrapperTests/InjectTests.swift
+++ Tests/SwinjectPropertyWrapperTests/InjectTests.swift
@@ -1,152 +1,141 @@
-import Quick
-import Nimble
+import Foundation
+import XCTest
 @testable import SwinjectPropertyWrapper
 import Swinject
+import CwlPreconditionTesting
 
-final class InjectTests: QuickSpec {
-
+final class InjectTests: XCTestCase {
+    
     private class TestService {}
-
+    
     private class TestClass {
         @Inject var service: TestService
     }
-
-    override func spec() {
-        describe("Inject") {
-            describe("resolving") {
-                context("global injection resolver not set") {
-                    it("should not throw a precondition failure before accessing value") {
-                        let service = TestService()
-
-                        let container = Container()
-                        container.register(TestService.self, factory: { _ in service })
-
-                        expect {
-                            let _ = TestClass()
-                        }.toNot(throwAssertion())
-                    }
-
-                    it("should throw a precondition failure") {
-                        let service = TestService()
-
-                        let container = Container()
-                        container.register(TestService.self, factory: { _ in service })
-
-                        expect {
-                            InjectSettings.resolver = nil
-                            let object = TestClass()
-                            let _ = object.service
-                        }.to(throwAssertion())
-                    }
-                }
-
-                context("global injection resolver is set") {
-                    var container: Container!
-
-                    beforeEach {
-                        container = Container()
-                        InjectSettings.resolver = container
-                    }
-
-                    context("service is not registered") {
-                        it("should not throw a precondition failure before accessing value") {
-                            expect {
-                                let _ = TestClass()
-                            }.toNot(throwAssertion())
-                        }
-
-                        it("should throw a precondition failure") {
-                            expect {
-                                let object = TestClass()
-                                let _ = object.service
-                            }.to(throwAssertion())
-                        }
-                    }
-
-                    context("service is registered") {
-                        let service = TestService()
-
-                        beforeEach {
-                            container.register(TestService.self, factory: { _ in service })
-                        }
-
-                        it("should not throw a precondition failure before accessing value") {
-                            expect {
-                                let _ = TestClass()
-                            }.toNot(throwAssertion())
-                        }
-
-                        it("should return registered service") {
-                            expect(TestClass().service) === service
-                        }
-
-                        it("should not initalize value more than once") {
-                            let object = TestClass()
-                            expect(object.service) === service
-                            expect(object.service) === service
-                        }
-                    }
-                }
-
-                context("custom resolver set") {
-                    it("should use custom resolver") {
-                        class CustomResolverTestSettings {
-                            static let container = Container()
-                        }
-                        class CustomResolverTestObject {
-                            @Inject(resolver: CustomResolverTestSettings.container) var service: TestService
-                        }
-
-                        let service = TestService()
-                        CustomResolverTestSettings.container.register(TestService.self, factory: { _ in service })
-
-                        let object = CustomResolverTestObject()
-                        expect(object.service) === service
-                    }
-
-                    context("custom service name") {
-                        it("should use custom name") {
-                            class CustomResolverTestSettings {
-                                static let container = Container()
-                            }
-                            class CustomResolverTestObject {
-                                @Inject(name: "MyAwesomeTestService", resolver: CustomResolverTestSettings.container) var service: TestService
-                            }
-
-                            let service = TestService()
-                            CustomResolverTestSettings.container.register(TestService.self, name: "MyAwesomeTestService", factory: { _ in service })
-
-                            let object = CustomResolverTestObject()
-                            expect(object.service) === service
-                        }
-
-                        it("should fail to resolve without name") {
-                            class CustomResolverTestSettings {
-                                static let container = Container()
-                            }
-                            class CustomResolverTestObject {
-                                @Inject(name: "MyAwesomeTestService", resolver: CustomResolverTestSettings.container) var service: TestService
-                            }
-
-                            let service = TestService()
-                            CustomResolverTestSettings.container.register(TestService.self, factory: { _ in service })
-
-                            let object = CustomResolverTestObject()
-                            expect{
-                                _ = object.service
-                            }.to(throwAssertion())
-                        }
-                    }
-                }
-            }
-            describe("setter") {
-                it("should set value and initalized state") {
-                    let object = TestClass()
-                    let service = TestService()
-                    object.service = service
-                    expect(object.service) === service
-                }
-            }
+    
+    @MainActor override func setUp() {
+        super.setUp()
+        // Reset resolver before each test
+        InjectSettings.resolver = nil
+    }
+    
+    @MainActor func testNoResolverSetShouldNotThrowBeforeAccess() {
+        let service = TestService()
+        let container = Container()
+        container.register(TestService.self, factory: { _ in service })
+        
+        // Should not throw when creating instance
+        _ = TestClass()
+    }
+    
+    @MainActor func testNoResolverSetShouldThrowOnAccess() {
+        let service = TestService()
+        let container = Container()
+        container.register(TestService.self, factory: { _ in service })
+        
+        let object = TestClass()
+        let e = catchBadInstruction {
+            _ = object.service
         }
+        XCTAssertNotNil(e)
+    }
+    
+    @MainActor func testUnregisteredServiceShouldNotThrowBeforeAccess() {
+        let container = Container()
+        InjectSettings.resolver = container
+        
+        // Should not throw when creating instance
+        _ = TestClass()
+    }
+    
+    @MainActor func testUnregisteredServiceShouldThrowOnAccess() {
+        let container = Container()
+        InjectSettings.resolver = container
+        
+        let object = TestClass()
+        let e = catchBadInstruction {
+            _ = object.service
+        }
+        XCTAssertNotNil(e)
+    }
+    
+    @MainActor func testRegisteredServiceResolution() {
+        let container = Container()
+        InjectSettings.resolver = container
+        
+        let service = TestService()
+        container.register(TestService.self, factory: { _ in service })
+        
+        let object = TestClass()
+        XCTAssertTrue(object.service === service)
+    }
+    
+    @MainActor func testServiceSingletonBehavior() {
+        let container = Container()
+        InjectSettings.resolver = container
+        
+        let service = TestService()
+        container.register(TestService.self, factory: { _ in service })
+        
+        let object = TestClass()
+        let firstAccess = object.service
+        let secondAccess = object.service
+        
+        XCTAssertTrue(firstAccess === secondAccess)
+        XCTAssertTrue(firstAccess === service)
+    }
+    
+    @MainActor func testCustomResolver() {
+        class CustomResolverTestSettings {
+            static let container = Container()
+        }
+        class CustomResolverTestObject {
+            @Inject(resolver: CustomResolverTestSettings.container) var service: TestService
+        }
+        
+        let service = TestService()
+        CustomResolverTestSettings.container.register(TestService.self, factory: { _ in service })
+        
+        let object = CustomResolverTestObject()
+        XCTAssertTrue(object.service === service)
+    }
+    
+    @MainActor func testCustomServiceName() {
+        class CustomResolverTestSettings {
+            static let container = Container()
+        }
+        class CustomResolverTestObject {
+            @Inject(name: "MyAwesomeTestService", resolver: CustomResolverTestSettings.container) var service: TestService
+        }
+        
+        let service = TestService()
+        CustomResolverTestSettings.container.register(TestService.self, name: "MyAwesomeTestService", factory: { _ in service })
+        
+        let object = CustomResolverTestObject()
+        XCTAssertTrue(object.service === service)
+    }
+    
+    @MainActor func testCustomServiceNameFailureWithoutName() {
+        class CustomResolverTestSettings {
+            static let container = Container()
+        }
+        class CustomResolverTestObject {
+            @Inject(name: "MyAwesomeTestService", resolver: CustomResolverTestSettings.container) var service: TestService
+        }
+        
+        let service = TestService()
+        CustomResolverTestSettings.container.register(TestService.self, factory: { _ in service })
+
+        let object = CustomResolverTestObject()
+        let e = catchBadInstruction {
+            _ = object.service
+        }
+        XCTAssertNotNil(e)
+    }
+    
+    @MainActor func testSetterBehavior() {
+        let object = TestClass()
+        let service = TestService()
+        object.service = service
+        XCTAssertTrue(object.service === service)
     }
 }
